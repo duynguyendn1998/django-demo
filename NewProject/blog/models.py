b1d342d5a0aff0bbf64a05606b1262a05cf74b36
Duy Nguyen <nptduy@tma.com.vn>
2021-12-03 11:22:20 +0700

    [Models + Database] add readme.md + update models use foreign key

--- NewProject/blog/models.py
+++ NewProject/blog/models.py
@@ -1,20 +1,23 @@
 from django.db import models
+from django.db.models.fields.reverse_related import ManyToOneRel
 
-# Create your models here.
+# Create Users models.
 class Users(models.Model):
-    user_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=50) # data type varchar with max_length 100
     email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    created_dt = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=50)
+    created_dt = models.DateTimeField(auto_now_add=True) # data type timestamptz and auto add when insert row
 
+# Create Posts models.
 class Posts(models.Model):
-    user_id: models.IntegerField()
+    user = models.ForeignKey(Users,on_delete=models.CASCADE, default="") # update to foreign key --> it is user_id in Posts table of database
     title = models.CharField(max_length=100)
-    content_post = models.TextField()
-    created_dt = models.DateTimeField(auto_now_add=True)
+    content_post = models.TextField() # data type text
+    created_dt = models.DateTimeField(auto_now_add=True) 
 
+# Create Comments models.
 class Comments(models.Model):
-    user_id: models.IntegerField()
-    post_id: models.IntegerField()
+    user = models.ForeignKey(Users,on_delete=models.CASCADE, default="")  # update to foreign key --> it is user_id in Comments table of database
+    post = models.ForeignKey(Posts,on_delete=models.CASCADE, default="")  # update to foreign key --> it is post_id in Comments table of database
     content_comment = models.TextField()
     created_dt = models.DateTimeField(auto_now_add=True)
